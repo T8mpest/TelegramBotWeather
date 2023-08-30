85ab5631b10fb468467bf273306b8a9a7d69631f
T8mpest <opcache@gmail.com>
2023-08-31 02:36:32 +0300

    new version

--- Program.cs
+++ Program.cs
@@ -11,13 +11,17 @@ using TelegramBotWeather;
 using Telegram.Bot.Types.InlineQueryResults;
 using System.Threading;
 using System;
+using Newtonsoft.Json;
+
 namespace TelegramBotWeather
 
 {
     class Program
     {
         private const string BotToken = "6154663885:AAFfH8MfyJ263NGUvEFipcTkLD1Hx46a2rY";
-        private const string WeatherApiKey = "https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=ca085266e256c19f8ad8a74dbcfe86e2";
+        private const string WeatherApiKey = "ca085266e256c19f8ad8a74dbcfe86e2";
+        private static Dictionary<long, string> CityRequests = new Dictionary<long, string>();
+        
 
         private static readonly HttpClient httpClient = new HttpClient();
 
@@ -38,6 +42,7 @@ namespace TelegramBotWeather
             Console.ReadLine();
             cts.Cancel();
         }
+        
 
 
 
@@ -50,24 +55,49 @@ namespace TelegramBotWeather
 
             if (message.Text == "/start")
             {
-                 await bot.SendStickerAsync(update.Message.Chat.Id, sticker: InputFile.FromUri("https://raw.githubusercontent.com/T8mpest/TelegramBotWeather/main/stickers/sticker.webp"), allowSendingWithoutReply: true, cancellationToken: ct);
+                 await bot.SendStickerAsync(update.Message.Chat.Id, sticker: InputFile.FromUri("https://raw.githubusercontent.com/T8mpest/TelegramBotWeather/main/stickers/sticker1.webp"), allowSendingWithoutReply: true, cancellationToken: ct);
             }
             else if (message.Text.StartsWith("/temperature"))
             {
-                var city = message.Text.Substring(12).Trim();
+                CityRequests[message.Chat.Id] = "waiting_for_city"; // Сохраняем состояние ожидания названия города
+                await bot.SendTextMessageAsync(message.Chat.Id, "Введите название города, чтобы получить прогноз погоды на 7 дней:");
+            }
+            else if (CityRequests.TryGetValue(message.Chat.Id, out var cityRequestState) && cityRequestState == "waiting_for_city")
+            {
+                CityRequests.Remove(message.Chat.Id); // Удаляем состояние запроса города
+
+                var city = message.Text.Trim();
+                Console.WriteLine($"Requested city: {city}"); // Выводим название города в консоль
                 var weatherInfo = await GetWeatherInfo(city);
 
                 if (weatherInfo != null)
                 {
-                    var response = $"Температура в городе Днепр {city}: {weatherInfo.Main.Temp}°C";
+                    var response = $"Прогноз погоды в городе {city}:\n";
+
+                    foreach (var forecast in weatherInfo.ForecastList)
+                    {
+                        var forecastDateTime = DateTimeOffset.FromUnixTimeSeconds(forecast.DateUnix);
+                        int[] targetHours = { 6, 12, 15, 18 };
+
+                        if (!targetHours.Contains(forecastDateTime.Hour))
+                        {
+                            continue;
+                        }
+
+                        
+                        response += $"{forecastDateTime.LocalDateTime.ToShortDateString()} {forecastDateTime.LocalDateTime.ToShortTimeString()}: {forecast.Main.Temp}°C\n";
+                    }
+
                     await bot.SendTextMessageAsync(message.Chat.Id, response);
                 }
                 else
                 {
-                    await bot.SendTextMessageAsync(message.Chat.Id, "Извините, не удалось получить данные о погоде.");
+                    await bot.SendTextMessageAsync(message.Chat.Id, "Извините, но идите вы нахуй с такими запросами далбоебы блять!❤️");
+                    await bot.SendStickerAsync(update.Message.Chat.Id, sticker: InputFile.FromUri("https://raw.githubusercontent.com/T8mpest/TelegramBotWeather/main/stickers/sticker.webp"), allowSendingWithoutReply: true, cancellationToken: ct);
                 }
             }
-            else if (message.Text.StartsWith("/mytemperature"))
+
+            if (message.Text.StartsWith("/mytemperature"))
             {
                 if (message.Location != null)
                 {
@@ -75,7 +105,7 @@ namespace TelegramBotWeather
 
                     if (weatherInfo != null)
                     {
-                        var response = $"Текущая температура у вас: {weatherInfo.Main.Temp}°C";
+                        var response = $"Текущая температура у вас: {weatherInfo.ForecastList}°C";
                         await bot.SendTextMessageAsync(message.Chat.Id, response);
                     }
                     else
@@ -94,6 +124,7 @@ namespace TelegramBotWeather
         {
             
             var url = $"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=ca085266e256c19f8ad8a74dbcfe86e2&units=metric";
+            Console.WriteLine($"Request URL: {url}");
 
             try
             {
@@ -118,8 +149,8 @@ namespace TelegramBotWeather
         }
         private static async Task<WeatherResponse> GetWeatherInfo(string city)
         {
-            var url = $"http://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=ca085266e256c19f8ad8a74dbcfe86e2&units=metric";
-
+            var url = $"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={WeatherApiKey}&units=metric";
+            Console.WriteLine($"Request URL: {url}");
             try
             {
                 using var response = await httpClient.GetAsync(url);
@@ -158,11 +189,22 @@ namespace TelegramBotWeather
 
     class WeatherResponse
     {
-        public WeatherMain Main { get; set; }
+        [JsonProperty("list")]
+        public List<WeatherForecast> ForecastList { get; set; }
     }
 
-    class WeatherMain
+    class WeatherForecast
     {
+        [JsonProperty("dt")]
+        public long DateUnix { get; set; }
+
+        [JsonProperty("main")]
+        public TemperatureInfo Main { get; set; }
+    }
+
+    class TemperatureInfo
+    {
+        [JsonProperty("temp")]
         public float Temp { get; set; }
     }
 }
